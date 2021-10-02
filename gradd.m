baf6c1e5c37dd84b15139b0ed5acacca37aa12d7
Lang Song <will0216sl@gmail.com>
2021-10-02 19:30:01 -0400

    Updated gradd.m

--- gradd.m
+++ gradd.m
@@ -1,7 +1,10 @@
-as = (3* cos(pi/5)+cos(3*pi/10) - cos(2*pi/5))/ pi;
+%% I collaborated with Zhirui Li on the inplementation of the GD and SG algorithms
+%% Main driver
+
+%the global minimum
+as = (3* cos(pi/5)+cos(3*pi/10) - cos(2*pi/5))/ pi; 
 bs = (1.4 * pi *a_prob1-4+cos(pi/5)+cos (3* pi /10)+cos (2* pi / 5 ) ) / 4;
 % disp(f(as,bs));
-
 
 pa = fp(1,0);
 pb = fb(1,0);
@@ -10,54 +13,30 @@ pb = fb(1,0);
 alpha_s = (pi/2)/(-pb + (pi/2) *pa);
 
 %Call gradient descend function
-[as,bs] = graddes(0.99*alpha_s);
+[a_list,b_list] = graddes(0.99*alpha_s);
+
+%Show the result
 close all
+figure(1)
+hold on
+xlabel("iter");
+ylabel("value");
+plot(1:100001, a_list, 1:100001, b_list)
+legend('a', 'b')
+
+
+%Run the SGD algorithm
+[iter, a_list,b_list] = sgrad(0.5);
+
+%Show the result
 figure(2)
 hold on
 xlabel("iter");
 ylabel("value");
-plot(1:100001, as, 1:100001, bs)
+plot(1:iter+1, a_list, 1:iter+1, b_list)
 legend('a', 'b')
 
-for j = 1:10
-    msgmain = ['Begin ', num2str(j), 'th SGD simulation'];
-    disp(msgmain)
-    sgrad()
-    disp('SGD finished')
-    disp('-------')
-end
-
-%%plots
-% close all
-% figure(1)
-% hold on
-% yl = [-4,4];
-% xl = [-4,4];
-% xBox = [0, xl(1), xl(1), 0, 0];
-% yBox = [0 ,0, yl(2), yl(2), 0];
-% patch(xBox,yBox,'red' , 'FaceColor' , 'green');
-% xs = 0:0.5:10;
-% line1 = pi * xs / 2 ;
-% xTri = [0,0 ,10/pi , 0] ;
-% yTri = [0 ,yl(2) , yl(2) , 0];
-% patch( xTri , yTri , 'red' , 'FaceColor' , 'green' ) ;
-% a_prob1 = (3* cos(pi/5)+cos(3*pi/10) - cos(2*pi/5))/ pi;
-% b_prob1 = (1.4 * pi *a_prob1-4+cos(pi/5)+cos (3* pi /10)+cos (2* pi / 5 ) ) / 4;
-% scatter ( a_prob1 , b_prob1 , "filled")
-% xlabel (" a ")
-% ylabel ("b")
-
-c = -5:1:5;
-d = c;
-[a,b] = meshgrid(c);
-x1 = 0;
-x2 = pi/10;
-x3 = 2*pi/10;
-x4 = 3*pi/10;
-x5 = 4*pi/10;
-x6 = 5*pi/10;
-
-%%definition of functions
+%% definition of functions and algorithms
 function y = g(x)
     y = 1 - cos(x);
 end
@@ -82,7 +61,7 @@ function z = fb(a,b) %gradient of f over b
     z = (1/6)*((x1*a - b>0)*(-1)*(x1*a - b - g(x1)) + (x2*a - b>0)*(-1)*(x2*a - b - g(x2)) + (x3*a - b>0)*(-1)*(x3*a - b - g(x3)) + (x4*a - b>0)*(-1)*(x4*a - b - g(x4)) + (x5*a - b>0)*(-1)*(x5*a - b - g(x5)) + (x6*a - b>0)*(-1)*(x6*a - b - g(x6)));
 end
 
-function[gradlossa,gradlossb] = gradlosstrain(j,a,b)
+function[gradlossa,gradlossb] = gradtrain(j,a,b)  %function used for SGD algorithm
 x1 = 0;
 x2 = pi/10;
 x3 = 2*pi/10;
@@ -110,7 +89,7 @@ else
 end
 end
 
-function t = f(a,b) %the loss function
+function t = f(a,b) %the loss function f(a,b)
     x1 = 0;
     x2 = pi/10;
     x3 = 2*pi/10;
@@ -120,80 +99,73 @@ function t = f(a,b) %the loss function
     t = (1/12)*((max(0, (x1*a - b)) - g(x1))^2 + (max(0, (x2*a - b)) - g(x2))^2 + (max(0, (x3*a - b)) - g(x3))^2 + (max(0, (x4*a - b)) - g(x4))^2 + (max(0, (x5*a - b)) - g(x5))^2 + (max(0, (x6*a - b)) - g(x6))^2);
 end
 
-function [alist, blist] = graddes(alpha) %gradient descent algorithm
-%     for alpha = alphalist
-%         dmsg = ['Current alpha = ', num2str(alpha)];
-%         disp(dmsg);
-        a = 1;
-        b = 0;
-        max_iter = 100000;
-        iter = 0;
-        alist = nan(max_iter, 1);
-        blist = nan(max_iter, 1);
-        while iter < max_iter && norm([fp(a,b),fb(a,b)]) > 1e-8
-            iter = iter + 1;
-            alist(iter) = a;
-            blist(iter) = b;
-            grad_a = fp(a,b);
-            grad_b = fb(a,b);
-            a = a - alpha*grad_a;
-            b = b - alpha*grad_b;
-        end
-        disp([a,b]);
-        disp(f(a,b));
-        disp([fp(a,b), fb(a,b)]);
-        disp(iter);
-        disp('----------------------------');
-        
-        alist(iter+1) = a;
-        blist(iter+1) = b;
-        alist = alist(1:(iter+1));
-        blist = blist(1:(iter+1));
-    %end
-end
-
-function [] = sgrad() %stochastic gradient descent algorithm
-    a=1;
-    b=0;
-    alpha0 = 0.5;
-    max_iter = 100000;
-    iter = 1; 
-    alist = zeros(1,max_iter);
-    blist = zeros(1,max_iter);
-    alist(iter) = a;
-    blist(iter) = b;
-    m0 = 150; 
-    iter = iter +1;
-    alpha = alpha0;
-    j = randi([0,5]);
-    [grad_a, grad_b] = gradlosstrain(j,a,b);
+%gradient descent algorithm
+function [a_list, b_list] = graddes(alpha)
+a = 1;
+b = 0;
+max_iter = 100000;
+iter = 0;
+a_list = zeros(1,max_iter);
+b_list = zeros(1,max_iter);
+while iter < max_iter && norm([fp(a,b),fb(a,b)]) > 1e-8
+    iter = iter + 1;
+    a_list(iter) = a;
+    b_list(iter) = b;
+    grad_a = fp(a,b);
+    grad_b = fb(a,b);
     a = a - alpha*grad_a;
     b = b - alpha*grad_b;
-    alist(iter) = a;
-    blist(iter) = b;
-    while iter < max_iter && (norm(alist(iter)-alist(iter-1), blist(iter)-blist(iter-1)) > 1e-11)
+end
+a_list(iter+1) = a;
+b_list(iter+1) = b;
+a_list = a_list(1:(iter+1));
+b_list = b_list(1:(iter+1));
+end
+
+%stochastic gradient descent algorithm
+function [iter, a_list,b_list] = sgrad(alpha0) 
+    a=1;
+    b=0;
+    max_iter = 100000;
+    r_size = 150; 
+    iter = 1; 
+    a_list = zeros(1,max_iter);
+    b_list = zeros(1,max_iter);
+    a_list(iter) = a;
+    b_list(iter) = b; 
+    iter = iter +1;
+    j = randi([0,5]);
+    [grad_a, grad_b] = gradtrain(j,a,b);
+    a = a - alpha0*grad_a;
+    b = b - alpha0*grad_b;
+    a_list(iter) = a;
+    b_list(iter) = b;
+    while iter < max_iter && (norm(a_list(iter)-a_list(iter-1), b_list(iter)-b_list(iter-1)) > 1e-11)
         iter = iter + 1;
-        if iter <=m0
+        if iter <= r_size  %for the first r_size iterations, use initial stepsize
             alpha = alpha0;
-        elseif iter <= 2*m0
-        alpha = alpha0/2;
-        else
-            index = iter - 2*m0;
-            klist = 2:40;
-            seq = idivide(int64(2.^klist), int64(klist));
-            indexint = m0.*cumsum(seq);
-            kk = find(indexint > index, 1, 'first');
-            alpha = alpha0/(2^(kk+1));
+        elseif iter <= 2*r_size  %for the next r_size iterations, use alpha0/2 stepsize
+            alpha = alpha0/2;
+        else  %if current iteration is greater than 2*r_size
+            cur_iter = iter - 2*r_size;
+            index = 2:40;
+            factor = idivide(int64(2.^index), int64(index));
+            round_length = r_size.*cumsum(factor);  %thresholds that determine when to reduce stepsize
+            round_index = find(round_length > cur_iter, 1, 'first');  %find the corresponding round index the current iteration is on
+            alpha = alpha0/(2^(round_index+1));
         end
         j = randi([0,5]);
-        [grad_a, grad_b] = gradlosstrain(j,a,b);
+        [grad_a, grad_b] = gradtrain(j,a,b);
         a = a - alpha*grad_a;
         b = b - alpha*grad_b;
-        alist(iter) = a;
-        blist(iter) = b;
+        a_list(iter) = a;
+        b_list(iter) = b;
     end
-    alist(end) = a;
-    blist(end) = b;
+    a_list(iter+1) = a;
+    b_list(iter+1) = b;
+    a_list = a_list(1:(iter+1));
+    b_list = b_list(1:(iter+1));
+    
     disp(['Result: ', 'a= ', num2str(a), ', b= ', num2str(b)]);
     disp(['Loss= ',  num2str(f(a,b))]);
 end
